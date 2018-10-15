5b4b4f5951530a8dea4bdc4ceb53ab3b74ea3bc6
shannen edwin <sedwin@linux60411.dc.engr.scu.edu>
2018-10-15 16:11:31 -0700

    moved ip config script to its own directory

--- server/test.js
+++ server/test.js
@@ -1,7 +1,7 @@
 var http = require('http'),
 	fs = require('fs')
 
-var ip = fs.readFileSync("ip.txt").toString().split('\n')[0]
+var ip = fs.readFileSync("ip/ip.txt").toString().split('\n')[0]
 
 var host = 'http://' + ip + ':60818/'
 http.get(host, (resp) => { //'http://129.210.16.71:60818/', (resp) => {
