8b0feb2d1390fcf4da47c1a05e5aa99f11b5e300
Sarah Johnson <sajohnson2468@gmail.com>
2018-10-21 18:24:45 -0700

    Added denied field to data splitting

--- src/api/EventUtils.js
+++ src/api/EventUtils.js
@@ -2,13 +2,15 @@ const splitEvents = data => {
     const date = new Date();
     const upcoming = [], past = [], pending = [];
     data.forEach(event => {
-        if (!event.approved) {
-            pending.push(event);
-        }
-        else if (event.date > date) {
-            upcoming.push(event);
-        } else {
-            past.push(event);
+        if (!event.denied) {
+            if (!event.approved) {
+                pending.push(event);
+            }
+            else if (event.date > date) {
+                upcoming.push(event);
+            } else {
+                past.push(event);
+            }
         }
     });
     return {
