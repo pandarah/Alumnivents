965fc4723d3245250c7a3264769894b7ff5cd41a
Sarah Johnson <sajohnson2468@gmail.com>
2018-10-21 20:28:10 -0700

    Updated example data and table recommendation

--- ExampleData.js
+++ ExampleData.js
@@ -1,64 +1,83 @@
 
 /**
  * @file Suggested Database Structure
- * @summary I suggest we use 3 seperate tables
+ * @summary I suggest we use 5 seperate tables
  *  - Event Table
+ *  - Host Table
+ *  - Location Table
  *  - Attendee Table
  *  - Comment Table
  * 
  * Essentially we will store all of the events in a single table and the identifier will be the ID
- * - The list of attendees will be stored as a list of the IDs of attendees
- * - The list of comments will be stored as a list of IDs of comments
+ * The hosts will have its own table and have a reference ID to the event ID
+ * The locations will have its own table and have a reference ID to the event ID
+ * The attendees will have its own table and have a reference ID to the event ID
+ * The comments will have its own table and have a reference ID to the event ID
  * 
  * When accessing a single event, in order to get the attendees and comments, we should run JOIN sql commands
- * To search for the attendees/comments for a specific event. This is because you can't store arrays in a MySQL database
+ * to search for the attendees/comments for a specific event. This is because you can't store arrays in a MySQL database.
  * 
+ * For each event ID, we will search for the specified host, location, attendees and comments.
+ * 
+ * The required columns with column types can be found below.
  * 
  */
-const events = [
-    {
-        id: 0, // Number (auto incremented)
-        name: 'Grand Reunion', // String
-        host: 'Grant Smith', // String
-        location: 'Locatelli', // String
-        date: 1539954540, // Epoch Time Stamp
-        endTime: 1539954541, // Epoch Time Stamp
-        description: 'lorem ipsum', // String,
-        category: 'Sports', // String
-        attendees: [
-            3,
-            7,
-        ], // Array of Numbers (attendee IDs)
-        comments: [
-            0, 
-            23,
-        ], // Array of Numbers (comment IDs)
-        approved: true, // Boolean
-    },
-];
+const event = {
+    id: 0, // Number (auto incremented)
+    name: 'Grand Reunion', // String
+    date: 1539954540, // Epoch Time Stamp
+    endTime: 1539954541, // Epoch Time Stamp
+    description: 'lorem ipsum', // String,
+    category: 'Sports', // String
+    interested: 10, // Number (count of how many ppl are interested)
+    approved: true, // Boolean
+    denied: false, // Boolean
+    created: 1539954540, // Epoch Time Stamp
+};
 
-const attendees = [
-    {
-        id: 0, // Number (auto incremented)
-        name: 'Grant Smith', // String
-        major: 'Child Studies', // String
-        gradYear: 2000, // Number
-        created: 1539954540, // Epoch Time Stamp
-    },
-];
+const host = {
+    id: 0, // Number (auto incremented)
+    eventID: 0, // Number
+    name: 'Grant Smith', // String
+    major: 'Child Studies', // String
+    graduation: 2000, // Number
+    created: 1539954540, // Epoch Time Stamp
+};
 
-const comments = [
-    {
-        id: 0, // Number (auto incremented)
-        name: 'Grant Smith', //String
-        rating: 4, // Number (out of 5)
-        detail: 'lorem ipsum', //String
-        created: 1539954540, // Epoch time stamp
-    },
-]
+const location = {
+    id: 0, // Number (auto incremented)
+    eventID: 0, // Number
+    address: '500 El Camino Real', // String
+    city: 'Santa Clara', // String
+    state: 'California', // String
+    zipcode: 95053, // Number
+    country: 'United States', // String
+    created: 1539954540, // Epoch Time Stamp
+
+};
+
+const attendee = {
+    id: 0, // Number (auto incremented)
+    eventID: 0, // Number
+    name: 'Grant Smith', // String
+    major: 'Child Studies', // String
+    graduation: 2000, // Number
+    created: 1539954540, // Epoch Time Stamp
+};
+
+const comment = {
+    id: 0, // Number (auto incremented)
+    eventID: 0, // Number
+    name: 'Grant Smith', //String
+    rating: 4, // Number (out of 5)
+    detail: 'lorem ipsum', //String
+    created: 1539954540, // Epoch time stamp
+};
 
 module.exports = {
-    events,
-    attendees,
-    comments,
+    event,
+    host,
+    location,
+    attendee,
+    comment,
 };
